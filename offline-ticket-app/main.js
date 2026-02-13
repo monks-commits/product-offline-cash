12533154d6c2d30ffc85b5b475166450e0c4f2fa
monks-commits <monks@mail.ru>
2026-02-13 18:52:12 +0200

    Update main.js

--- offline-ticket-app/main.js
+++ offline-ticket-app/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-const Database = require("better-sqlite3");
+const Database = require("better-sqlite3"); 
 
 let db;
 
